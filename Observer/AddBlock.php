24e4add5ec11c07664dc9be78a98797b7a55c24a
Ryan <ryan.holton@stsonline.uk.com>
2019-01-09 09:24:45 +0000

    Fix issue relating to including slider via shortcode in CMS

--- Observer/AddBlock.php
+++ Observer/AddBlock.php
@@ -83,6 +83,7 @@ class AddBlock implements ObserverInterface
             $output         = $observer->getTransport()->getOutput();
 
             foreach ($this->helperData->getActiveSliders() as $slider) {
+              if (empty($slider->getLocation())) continue;
                 $locations = explode(",", $slider->getLocation());
                 foreach ($locations as $value) {
                     list($pageType, $location) = explode('.', $value);
